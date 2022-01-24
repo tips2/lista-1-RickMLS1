4a4f1b88b7977ba7a8ac52e23fdab7a4aaf7ed8a
Rick Santos <52300263+RickMLS1@users.noreply.github.com>
2022-01-24 14:44:27 -0300

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,38 @@
+Coca_Cola = False
+Fanta_Cola = False
+Sprite = False
+Adocicada = False
+Noz_de_cola = False
+Cafeina = False
+Acida = False
+
+
+def base_fatos():
+
+    global Noz_de_cola
+    Noz_de_cola = True
+
+if __name__ == '__main__':
+
+    base_fatos()
+
+    for i in range(0, 10):
+
+        if Noz_de_cola and Cafeina:
+            Coca_Cola = True
+
+        if Noz_de_cola and Fanta_Cola:
+            Adocicada = True
+
+        if Acida:
+            print(f'Ácida = sim\n')
+            break
+
+        if Coca_Cola and Adocicada:
+            Acida = True
+
+        if Noz_de_cola:
+            Sprite = True
+            Fanta_Cola = True
+    else:
+        print(f'Ácida = não\n')
